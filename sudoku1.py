47385d8b18b431613698af8d65cb8580b13c3aa5
RallyGrote <rallygrote@gmail.com>
2026-04-03 18:17:13 -0500

    1 and 2 done

---
+++ sudoku1.py
@@ -0,0 +1,20 @@
+
+import sys
+import clingo
+
+
+class ClingoApp(clingo.application.Application):
+    def main(self, ctl, files):
+        for f in files:
+            ctl.load(f)
+        if not files:
+            ctl.load("-")
+        ctl.load("sudoku.lp")
+        ctl.ground()
+        ctl.solve()
+        
+    def print_model(self, model, printer):
+        atoms = sorted(model.symbols(shown=True), key=str)
+        print(" ".join(str(atom) for atom in atoms))
+        
+clingo.application.clingo_main(ClingoApp())
--- sudoku_board.py
+++ sudoku_board.py
@@ -1,14 +1,25 @@
 from typing import Tuple
 import clingo
 
-
 class Sudoku:
     def __init__(self, sudoku: dict[Tuple[int, int], int]):
         self.sudoku = sudoku
-
+        
     def __str__(self) -> str:
         s = ""
-        # YOUR CODE HERE
+        for row in range(9):
+            for column in range(9):
+                s += self.sudoku[(row+1, column+1)]
+                s += " "
+                
+                if column == 8:
+                    s += "\n"
+                    if (row + 1) % 3 == 0:
+                        s += "\n"
+                        
+                if (column + 1) % 3 == 0:
+                    s += " "
+        
         return s
 
     @classmethod
@@ -20,5 +31,9 @@ class Sudoku:
     @classmethod
     def from_model(cls, model: clingo.solving.Model) -> "Sudoku":
         sudoku = {}
+        positions = model.symbols(shown=True)
+        for position in positions:
+            positionString = str(position)
+            sudoku[(int(positionString[7]), int(positionString[9]))] = int(positionString[11])
         # YOUR CODE HERE
         return cls(sudoku)
