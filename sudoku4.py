4b9c7221f7e0573da2900ba476431747fff158fc
RallyGrote <rallygrote@gmail.com>
2026-04-03 19:05:21 -0500

    test

---
+++ sudoku4.py
@@ -0,0 +1,20 @@
+
+import sys
+import clingo
+from sudoku_board import Sudoku
+
+class ClingoApp(clingo.application.Application):
+    def main(self, ctl, files):
+        for f in files:
+            ctl.load(f)
+        if not files:
+            ctl.load("-")
+        ctl.load("sudoku.lp")
+        ctl.ground()
+        ctl.solve()
+        
+    def print_model(self, model, printer):
+        atoms = sorted(model.symbols(shown=True), key=str)
+        print(Sudoku.from_model(model))
+        
+clingo.application.clingo_main(ClingoApp())
